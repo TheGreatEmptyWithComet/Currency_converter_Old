682081683aa1bf8f89d03aeaa725ba9923b167f7
DonKihot <donkihot1984@gmail.com>
2023-09-15 21:28:55 +0300

    test class added

---
+++ WpfAppSimpleProject/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppSimpleProject
+{
+    internal class TestClass
+    {
+    }
+}
